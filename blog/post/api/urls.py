712b8bcb84a105718ef56cd8babf748f5b23c22a
Gizem Çırıkka <gizemtuncer17@gmail.com>
2022-09-28 16:46:01 +0300

    first

---
+++ blog/post/api/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path,include
+
+from post.api.views import PostListAPIView
+
+urlpatterns = [
+    path('list', PostListAPIView.as_view(), name='list')
+]
+
---
+++ blog/post/api/views.py
@@ -0,0 +1,5 @@
+from rest_framework.generics import ListAPIView
+from post.models import  Post
+
+class PostListAPIView(ListAPIView):
+    queryset = Post.objects.all()
