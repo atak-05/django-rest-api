bb88085f4f17c303762e2acd67117a83177a1ee5
gizem <gizemtuncer17@gmail.com>
2023-01-31 16:15:06 +0300

    created RetrieveUpdateApiView Favourite

--- blog/favourite/api/urls.py
+++ blog/favourite/api/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path,include
-from .views import FavouriteListCreateAPIView
 
+
+from .views import FavouriteAPIView,FavouriteListCreateAPIView
 
 app_name="favourite"
 urlpatterns = [
     path('list-create/', FavouriteListCreateAPIView.as_view(), name='list-create'),
+    path('update-delete/<pk>', FavouriteAPIView.as_view(), name='update-delete'),
 ]
 
--- blog/favourite/api/views.py
+++ blog/favourite/api/views.py
@@ -1,10 +1,13 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from favourite.api.serializers import FavouriteAPISerializer
+from favourite.api.paginations import FavouritePagination
 from favourite.models import Favourite
-from .serializers import FavouriteListCreateAPISerializer
+from favourite.api.serializers import FavouriteListCreateAPISerializer
 
 class FavouriteListCreateAPIView(ListCreateAPIView):
-    queryset = Favourite.objects.all()
+    # queryset = Favourite.objects.all()
     serializer_class = FavouriteListCreateAPISerializer
+    pagination_class = FavouritePagination
 
     def get_queryset(self):
         return Favourite.objects.filter(user = self.request.user)
@@ -12,3 +15,8 @@ class FavouriteListCreateAPIView(ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
 
+class FavouriteAPIView(RetrieveUpdateAPIView):
+    queryset = Favourite.objects.all()
+    serializer_class = FavouriteAPISerializer
+    lookup_field= 'pk'
+    
