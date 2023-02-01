422139d8b9f4a9112944b26c55b8d90d91f4edb3
gizem <gizemtuncer17@gmail.com>
2023-02-01 10:06:09 +0300

    Authorization created(jwt)

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt import views as jwt_views #Kullanıcı girişi için token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/post/', include('post.api.urls', namespace= 'post') ),#namepace HyperlinkedIdentityField da parametre olarak kullanılıyor.
     path('api/comment/', include('comment.api.urls', namespace= 'comment') ),
     path('api/favourite/', include('favourite.api.urls', namespace= 'favourite') ),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
 
 ] + static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # görselleri açmak için yapılan bir işlem
--- blog/favourite/api/views.py
+++ blog/favourite/api/views.py
@@ -4,11 +4,13 @@ from favourite.api.serializers import FavouriteAPISerializer
 from favourite.api.paginations import FavouritePagination
 from favourite.models import Favourite
 from favourite.api.serializers import FavouriteListCreateAPISerializer
+from rest_framework.permissions import IsAuthenticated
 
 class FavouriteListCreateAPIView(ListCreateAPIView):
     # queryset = Favourite.objects.all()
     serializer_class = FavouriteListCreateAPISerializer
     pagination_class = FavouritePagination
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Favourite.objects.filter(user = self.request.user)
@@ -17,6 +19,7 @@ class FavouriteListCreateAPIView(ListCreateAPIView):
         serializer.save(user = self.request.user)
 
 # Hem yorumu gösteriyor(GET), hem PUT işlemi, Hemde DEL işlemi  yapmamızı sağlıyor.... 
+# RetrieveUpdateDestroyAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView...
 class FavouriteAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Favourite.objects.all()
     serializer_class = FavouriteAPISerializer
