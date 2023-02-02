4d2f597b0dee4727c975281f4dd4d4b1d5168895
gizem <gizemtuncer17@gmail.com>
2023-02-02 21:40:15 +0300

    Authorization  editedd(jwt)

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 # Application definition
 
 INSTALLED_APPS = [
