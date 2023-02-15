ff36f2f1e846257da5385709ac70621fe5df14b9
gizem <gizemtuncer17@gmail.com>
2023-02-15 18:21:48 +0300

    Access token lifetime is created

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,6 +33,12 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+SIMPLE_JWT = {
+    'ACESS_TOKEN_LIFETIME' : timedelta(minutes=15)
+}
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
